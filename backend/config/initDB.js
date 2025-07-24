843c685d49f52c3d5f1c9c94b78bbda093a5f334
LouisPolicarpio <louis.policarpio@gmail.com>
2025-07-24 17:24:57 +1000

    backend init

--- backend/config/initDB.js
+++ backend/config/initDB.js
@@ -2,7 +2,6 @@ import {sql} from './db.js';
 
 export async function initDB() {
     try {
-        // Example query to create a table
         await sql`
             CREATE TABLE IF NOT EXISTS schedule (
                 id SERIAL PRIMARY KEY,
@@ -12,22 +11,24 @@ export async function initDB() {
                 startWeight INT NOT NULL,
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             );
-
-            CREATE TABLE IF NOT EXISTS  Week(
+            `;
+        await sql`
+            CREATE TABLE IF NOT EXISTS  week(
                 id SERIAL PRIMARY KEY,
                 weekNumber INT NOT NULL,
                 scheduleId INT REFERENCES schedule(id) ON DELETE CASCADE,
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             );
+        `;
 
-            CREATE TABLE IF NOT EXISTS  Day(
+        await sql`
+            CREATE TABLE IF NOT EXISTS  day(
                 id SERIAL PRIMARY KEY, 
                 actualWeight INT NOT NULL,
-                week_id INT REFERENCES Week(id) ON DELETE CASCADE,
+                week_id INT REFERENCES week(id) ON DELETE CASCADE,
                 week_day TEXT NOT NULL,
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             );
-
         `;
         console.log('Database initialized successfully');
     } catch (error) {
